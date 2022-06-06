07c3bc246828613ec2b53ec0db0a6726fe88d6d1
Alberto <albertopimentel04@gmail.com>
2022-06-06 18:30:45 -0500

    Configuracion de CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import enlaceRoutes from "./routes/enlaceRoutes.js";
@@ -12,6 +13,23 @@ app.use(express.json());
 dotenv.config();
 
 conectarDB();
+
+//Configuracion de CORS
+const whiteList = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      //Puede consultar la API
+      callback(null, true);
+    } else {
+      //No puede consultar la API
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 4000;
 
